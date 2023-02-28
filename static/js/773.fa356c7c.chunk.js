b40dfb1407a786dfdd762f25f87beebbc80fa999
Serhii <beschasnuy96@gmail.com>
2023-02-28 21:25:05 +0200

    Updates

---
+++ static/js/773.fa356c7c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkauth_medium=self.webpackChunkauth_medium||[]).push([[773],{2773:function(u,a,e){e.r(a);e(2791);var t=e(184);a.default=function(u){var a=u.image;return(0,t.jsx)("img",{src:a,alt:"lazy load"})}}}]);
+//# sourceMappingURL=773.fa356c7c.chunk.js.map
